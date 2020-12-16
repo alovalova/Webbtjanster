230318dfc9e0c4757c3e0087755a5f195cd485cc
ChanonBorg <60186836+ChanonBorg@users.noreply.github.com>
2020-12-16 15:24:36 +0100

    Tillagd controllerklass med mer struktur

--- Group20/src/main/java/APIRunner.java
+++ Group20/src/main/java/APIRunner.java
@@ -1,48 +1,43 @@
 import com.google.gson.Gson;
-import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import kong.unirest.HttpResponse;
 import kong.unirest.JsonNode;
 import kong.unirest.Unirest;
+import kong.unirest.json.JSONArray;
+import kong.unirest.json.JSONObject;
+
+import java.util.Calendar;
 
 import static spark.Spark.get;
 
 /**
- * @author
+ * @author Chanon Borgström % Sofia Hallberg
  * @created 09/12/2020
  * @project Group20
  */
 public class APIRunner {
+
+    JsonParser parser = new JsonParser();
+    Gson gson = new Gson();
+    APIController controller = new APIController();
+
     public static void main(String[] args) {
+        APIRunner runner = new APIRunner();
 
-        JsonParser parser = new JsonParser();
-        Gson gson = new Gson();
-        Parcel parcel = new Parcel();
+        get("/", (request, response) -> {
+            System.out.println("Are we there yet?");
 
-        get("/", (request, response) -> { // för vår server som får indata från webbclienten
-            // skapa och lägg in info om paket.
+
+            //ska hämta aktuell information om ett paket.
+//            boolean parcelCreated = runner.controller.createParcel();
+//            if(parcelCreated){
+//                //do something
+//            }
+
             return "";
+
         });
 
-        Unirest.config().defaultBaseUrl("http://api2.postnord.com/rest/transport"); // för anrop till andras APIer.
-
-
-        HttpResponse <JsonNode> response = Unirest.get("/v1/transittime/getTransitTimeInformation.json")
-                .queryString("apikey", "673d1dcaedbcd206cf3130c3cd01bb7d")
-                .queryString("dateOfDeparture", parcel.getParcelDepartureTime())
-                .queryString("serviceCode", "18")
-                .queryString("serviceGroupCode", parcel.getDepartureCountry())
-                .queryString("fromAddressPostalCode", parcel.getDepartureZip())
-                .queryString("fromAddressCountryCode", parcel.getDepartureCountry())
-                .queryString("toAddressPostalCode",parcel.getArrivalZip())
-                .queryString("toAddressCountryCode",parcel.getArrivalCountry())
-
-                .header("Accept", "application/json")
-                .asJson()
-
-        ;
-
-        System.out.println("response: " + response.getBody());
 
 
     }
